ce57867de921f59ec7a5d671a9f5317ba37adc18
0xPaulo <paulo_czar@outlook.com.br>
2022-09-01 23:55:53 -0300

    Ja ta logando no tumblr

---
+++ Automatizando tarefas/selenium1.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium. webdriver. common. keys import Keys
+import time
+driver = webdriver.Chrome(
+    executable_path=r'C:\Users\Paulo\vs\python\automatize tarefas\py\Automatizando tarefas\chromedriver.exe')
+driver.get('https://www.tumblr.com/dashboard')
+time.sleep(1)
+# 1 //*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/div[1]/input
+# 2 //*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/div[2]/input
+# 3 //*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/button/span
+email = driver.find_element(
+    By.XPATH, '//*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/div[1]/input')
+senha = driver.find_element(
+    By.XPATH, '//*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/div[2]/input')
+login = driver.find_element(
+    By.XPATH, '//*[@id="Get started"]/div/div/div[2]/div[1]/section/div/form/button/span')
+email.send_keys("caoslpure@gmail.com")
+senha.send_keys("J&nD3)e)J6emPn")
+time.sleep(1)
+login.click()
+time.sleep(5)
+driver.get('https://www.tumblr.com/new/photo?context=queue&name=inugamidoo')
+up = driver.find_element(By.CLASS_NAME, "TRX6J MD5Ec C_g5d")
+up.click()
